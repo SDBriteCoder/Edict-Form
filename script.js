dac0d53ef8d39cdfff5aac6544b6da941a111ce9
SDBriteCoder <93184052+SDBriteCoder@users.noreply.github.com>
2021-11-29 22:07:24 +0530

    commit
    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function form_validate(){
+    var regex = /^[a-zA-Z]{0,14}$/;
+    let name = document.forms["animi"]["name"].value;
+    let epi = document.forms["animi"]["episodes"].value;
+    if (name == "")
+    {
+        alert("Empty");
+        return false;
+    }
+    else if(name.length < 8)
+    {
+        alert("Name length cannot be lesser than 8 characters!");
+        return false;
+    }
+    else if(name.length >15)
+    {
+        alert("Name length cannot be greater than 15 characters!");
+        return false;
+    }    
+    else if(!regex.test(name))
+    {
+        alert("Name cannot have characters other than alphabets!");
+        return false;
+    }
+    alert("Hurray, the data is Accepted!");
+    return true;     
+}
